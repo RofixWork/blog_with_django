37c983cd6c7861e6c438882b45f7ad9600ac670e
Rofix <wourkout123@gmail.com>
2024-11-04 15:47:30 +0100

    Save user information in database, and redirect user to login page

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("register/", views.register, name="auth.register")]
+urlpatterns = [path("register/", views.register, name="auth.register"), path("login/", views.login, name="auth.login")]
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .forms import RegsiterForm
 
 
 # Create your views here.
 def register(request: HttpRequest) -> HttpResponse:
-    form = RegsiterForm()
+    if request.method == "POST":
+        form = RegsiterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("auth.login"))
+    else:
+        form = RegsiterForm()
     return render(request, "accounts/register.html", {"form": form})
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return render(request, "accounts/login.html")
