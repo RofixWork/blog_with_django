6f007a32e28990186a0aeec5843c18430b4c09ec
Rofix <wourkout123@gmail.com>
2024-11-04 17:23:26 +0100

    edit function handle_post_image code

--- blog/static/main.js
+++ blog/static/main.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', main)
 
 function main() {
-    handle_file()
+    handle_post_image()
 }
 
-function handle_file() {
-    file = document.querySelector('.post_image');
-    file.addEventListener('change', e => {
-        image = e.target.files[0]
-        if (image) {
-            type = image.type.split('/')[1]
-
-            if (!['jpg', 'png', 'webp', 'jpeg'].includes(type)) {
-                alert(`not support this type: ${type}`)
-                return
+function handle_post_image() {
+    file = document.querySelector('.post_image') ?? null;
+    if (file) {
+        file.addEventListener('change', e => {
+            image = e.target.files[0]
+            if (image) {
+                type = image.type.split('/')[1]
+    
+                if (!['jpg', 'png', 'webp', 'jpeg'].includes(type)) {
+                    alert(`not support this type: ${type}`)
+                    return
+                }
+    
+                post_image_box = document.getElementById('post_image-box')
+    
+                url = URL.createObjectURL(image)
+                post_image_box.setAttribute('src', url)
+    
             }
-
-            post_image_box = document.getElementById('post_image-box')
-
-            url = URL.createObjectURL(image)
-            post_image_box.setAttribute('src', url)
-
-        }
-        
-    })
+        })
+    }
 }
