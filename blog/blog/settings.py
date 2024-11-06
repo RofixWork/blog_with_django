a4a9a6e9a36961b25ed905749bd2c85d591eb373
Rofix <wourkout123@gmail.com>
2024-11-06 18:35:16 +0100

    Final

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8gj3wvz$al*np4u1#-z+^1i@6_@=m+-=%!r^v1)q^go(mqmt+3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,7 +123,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
